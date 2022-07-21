04617eb6e4b7c9de70b518f25cbde629eb776d56
VinceDZiree <contact@vincentdesiree.fr>
2022-07-21 11:03:17 +0200

    Bonne base. Pré ajout de boutons

--- script.js
+++ script.js
@@ -15,15 +15,20 @@ let M = {
     x: 0,
     y: 0,
     v: 0, // La vélocité verticale, indispensable pour appliquer un effet de gravité.
-    size: 30,
+    j: -6, // Vitesse initial de saut en m/s
+    size: 75,
     isGrounded: false
 }
+
 
 // La Gravité (9.81 m/s²)
 let g = 9.81
 
-// La force des couisses ! (Vélocité du saut)
-let j = 25
+// Variable de mise à l'échelle
+let conv = 0
+
+// Vitesse de l'effet
+let speed = 1
 
 /**
  * Exécutée une seule fois, au chargement
@@ -35,15 +40,37 @@ function LoadGame(canvas, ctx) {
 
     // Hauteur de sol
     fHeight = scr.height - 25
-
+    
     // Mario va au sol, au milieu
     M.x = scr.width/2-M.size/2
     M.y = fHeight - M.size
 
+    // Prise en compte des pressions de touches non répétèes
     keyDown('ArrowUp', () => {
         if (M.isGrounded) {
-            M.v = -j
+            M.v = M.j * conv
         }
+    })
+    keyDown('NumpadAdd', () => {
+        if(isKeyDown('KeyT')){
+            M.size += 5
+        }
+        else if(isKeyDown('KeyS')){
+            M.j -= 1
+        }
+        else{
+            speed += 0.5
+        }
+    })
+    keyDown('NumpadSubtract', () => {
+        if(isKeyDown('KeyT')){
+            M.size -= 5
+        }
+        else if(isKeyDown('KeyS')){
+            M.j += 1
+        }
+        else{
+            speed -= 0.5        }
     })
 }
 
@@ -51,6 +78,9 @@ function LoadGame(canvas, ctx) {
  * Exécutée perpétuellement pour mettre à jour les données
  */
 function UpdateGame(deltaTime) {
+    // Mario mesure 1m55 en "vrai" donc la conversion m => pixel donne
+    conv = M.size / 1.55
+
     // Prise en compte des pressions de touche
     if (isKeyDown('ArrowLeft') && M.x > 0) {
         M.x -= 10
@@ -59,7 +89,7 @@ function UpdateGame(deltaTime) {
         M.x += 10
     }
 
-    M.y += M.v
+    M.y += M.v * deltaTime * speed
     M.isGrounded = M.y + M.size >= fHeight
     
     if(M.isGrounded) {
@@ -67,7 +97,7 @@ function UpdateGame(deltaTime) {
         M.y = fHeight - M.size
     }
     else{
-        M.v += g * Math.sqrt(deltaTime)
+        M.v += g * conv * deltaTime * speed
     }
 }
 
@@ -77,6 +107,8 @@ function UpdateGame(deltaTime) {
 function DrawGame(ctx) {
     drawFloor(ctx)
     drawMario(ctx, M)
+    drawRule(ctx)
+    drawValues(ctx)
 }
 /**
  * Dessine Mario !
@@ -102,4 +134,36 @@ function drawFloor(ctx){
         ctx.lineTo(i+25, fHeight)
     }
     ctx.stroke()
+}
+
+/**
+ * Dessine une règle à l'échelle à gauche
+ */
+function drawRule(ctx){
+    ctx.strokeStyle = "white"
+    ctx.fillStyle = "white"
+    ctx.lineWidth = 2
+    ctx.font = "15px sans"
+
+    let y = fHeight
+    let i = 1
+    while(y > 0){
+        y -= conv
+        ctx.beginPath()
+        ctx.moveTo(0, y)
+        ctx.lineTo(15,y)
+        ctx.stroke()
+
+        ctx.fillText(`${i} m`, 18, y)
+        i++
+    }
+}
+
+function drawValues(ctx){
+    ctx.fillStyle = "white"
+    ctx.font = "15px sans"
+
+    ctx.fillText(`Echelle: t + numpad +/- = ${M.size}`, scr.width - 350, 25)
+    ctx.fillText(`Vitesse saut: s + numpad +/- = ${M.j} m/s`, scr.width - 350, 50)
+    ctx.fillText(`Vitesse effet: numpad +/- = *${speed}`, scr.width - 350, 75)
 }
